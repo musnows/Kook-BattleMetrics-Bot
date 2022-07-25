fe99f9e657084542314730f03b83c6d385c9249c
ewait <s2522446388@163.com>
2022-07-25 17:42:27 +0800

    feat(main): update help command

--- code/BM_main.py
+++ code/BM_main.py
@@ -26,7 +26,7 @@ async def botmarket():
     headers = {'uuid':'fbb98686-91fe-46b5-be2c-cf146cccc822'}
     async with aiohttp.ClientSession() as session:
         await session.post(api, headers=headers)
-    
+
 
 ######################################################################################
 
@@ -47,10 +47,10 @@ async def world(msg: Message):
 async def Help(msg: Message):
     logging(msg)
     cm = CardMessage()
-    c3 = Card(Module.Header('查询指令为`/BM`or`/bm`'))
+    c3 = Card(Module.Header('目前bm小助手支持的指令如下！'))
     c3.append(Module.Divider())
     #实现卡片的markdown文本
-    c3.append(Module.Section(Element.Text('参数: 服务器名，游戏名，显示前几个搜索结果\n使用示例：`/BM 萌新 hll 4`\n显示游戏`hll`服务器中名称包含`萌新`的前4个结果\n',Types.Text.KMD)))
+    c3.append(Module.Section(Element.Text('服务器查询指令为`/BM`or`/bm`\n参数: 服务器名，游戏名，显示前几个搜索结果\n使用示例：`/BM 萌新 hll 4`\n显示游戏`hll`服务器中名称包含`萌新`的前4个结果\n\n`/py 玩家id 服务器id`查询玩家在该服务器游玩时长\n',Types.Text.KMD)))
     c3.append(Module.Divider())
     c3.append(Module.Section('有任何问题，请加入帮助服务器与我联系',
               Element.Button('帮助', 'https://kook.top/gpbTwZ', Types.Click.LINK)))
@@ -67,7 +67,7 @@ async def check(msg: Message, name: str, game: str,max:int = 3):
         async with aiohttp.ClientSession() as session:
             async with session.get(url) as response:
                 ret = json.loads(await response.text())
-        
+
         count = 1
         cm = CardMessage()
         for server in ret['data']:
@@ -90,7 +90,7 @@ async def check(msg: Message, name: str, game: str,max:int = 3):
                                     Types.Text.KMD))))
             cm.append(c)
             count += 1
-        
+
         await msg.reply(cm)
     except Exception as result:
         #await msg.reply("很抱歉，发生了一些错误!\n提示:出现json错误是因为查询结果不存在\n\n报错: %s"%result)
@@ -103,7 +103,7 @@ async def check(msg: Message, name: str, game: str,max:int = 3):
               Element.Button('帮助', 'https://kook.top/Lsv21o', Types.Click.LINK)))
         cm.append(c)
         await msg.reply(cm)
-    
+
 # 查看玩家在某个服务器玩了多久，需要玩家id
 @bot.command(name='py',aliases=['player'])
 async def player_check(msg: Message, player_id: str, server_id: str):
